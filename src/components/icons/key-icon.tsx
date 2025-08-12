4e49f0d80e3e31713499a7c3d7235088a29c5f4c
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-08-12 17:01:59 +0000

    Cria um svg chave pra gente colocar nesse sistema!

---
+++ src/components/icons/key-icon.tsx
@@ -0,0 +1,22 @@
+
+import * as React from 'react';
+
+export function KeyIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777z" />
+      <path d="M15.5 7.5l3 3L22 7l-3-3" />
+    </svg>
+  );
+}
