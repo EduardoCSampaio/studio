7a9254b827bcaed6a8dd6de0bc3e2cee910e8691
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-08-12 17:40:40 +0000

    Agora aumenta a logo na barra lateral

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ export function Logo() {
         alt="ECS Logo"
         width={140}
         height={40}
-        className="h-10 w-auto"
+        className="h-12 w-auto"
         priority
       />
     </div>
