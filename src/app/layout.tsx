9347823c492b639ab0d249a0a9430d54ef3906cd
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-08-12 17:48:56 +0000

    Minha logo pode ser um svg no navegador? para aparecer em cima bonitinho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from '@/hooks/use-auth';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Favicon } from '@/components/icons/favicon';
 
 export const metadata: Metadata = {
   title: 'ECS - Sistema PDV',
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Favicon />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
