4b65a3d4b4a853aa29231d2c3519dd4619ea010f
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-08-12 17:51:02 +0000

    Tem como colocar meu logo.png que está em public como favicon?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from '@/hooks/use-auth';
 import { ThemeProvider } from '@/components/theme-provider';
-import { Favicon } from '@/components/icons/favicon';
 
 export const metadata: Metadata = {
   title: 'ECS - Sistema PDV',
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Favicon />
+        <link rel="icon" href="/logo.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
